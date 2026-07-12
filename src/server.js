6181279926c4007902a7b8f6ef568ca519cc8b3f
Luky <tsmluky@gmail.com>
2026-07-12 07:27:16 +0200

    Allow panel update requests

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     if (ORIGINS.includes('*')) res.set('Access-Control-Allow-Origin', '*');
     else if (origin && ORIGINS.includes(origin)) res.set('Access-Control-Allow-Origin', origin);
     res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Panel-Token');
-    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.set('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     if (req.method === 'OPTIONS') return res.sendStatus(204);
     next();
 });
